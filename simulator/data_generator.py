729e5a00de75afb81afe1f35d997c75378a3da68
Lucas Hanson <murdoc@rosewater.local>
2020-05-13 14:23:44 -0700

    integer generate a WIP

--- simulator/data_generator.py
+++ simulator/data_generator.py
@@ -16,7 +16,7 @@ def float_generate(settings, last_value):
         value        = round(last_value + (min_distance * (calculations.random_direction() * random.betavariate(2,100))),5)
 
     else:
-        print("[!] float mode:" + mode + " not supported")
+        print("[!] float mode: '" + mode + "' not supported")
         exit()
 
     return value
@@ -26,8 +26,23 @@ def bool_generate(settings):
     return True
 
 # handle integer value
-def integer_generate(settings):
-    return True
+def integer_generate(settings, last_value):
+    mode = settings['mode']
+
+    if mode == 'random':
+        value = int(random.triangular(settings['from'], settings['average'], settings['to']))
+
+    elif mode == 'linear':
+        distance_1   = abs(last_value - settings['to'])
+        distance_2   = abs(last_value - settings['from'])
+        min_distance = distance_1 if distance_1 < distance_2 else distance_2
+        value        = int(last_value + (min_distance * (calculations.random_direction() * random.betavariate(2,100))))
+
+    else:
+        print("[!] integer mode: '" + mode + "' not supported")
+        exit()
+
+    return value
 
 # handle string value
 def string_generate(settings):
@@ -46,7 +61,9 @@ def generate(schema):
         elif type == 'string':
             string_generate(field_settings)
         elif type == 'integer':
-            integer_generate(field_settings)
+            value = integer_generate(field_settings,0)
+            print("[*] field: " + field)
+            print("[*] value: " + str(value))
         elif type == 'bool':
             bool_generate(field_settings)
         else:
