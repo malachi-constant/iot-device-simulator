2f3e4d7cbb6fe51f50f1ef5107362eda0d36c9ca
Lucas Hanson <murdoc@rosewater.local>
2020-05-12 20:52:03 -0700

    cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 
-# DynamoDB Table for Storing Simulation State
+# dynamodb table for storing simulation state
 resource "aws_dynamodb_table" "simulation_table-table" {
   name           = join("-", [var.prefix, "simulations"])
   hash_key       = "simulation-id"
@@ -16,22 +16,22 @@ resource "aws_dynamodb_table" "simulation_table-table" {
   }
 }
 
-# Autoscaling Group for Simulation Workers
+# autoscaling group for simulation workers
 resource "aws_autoscaling_group" "simulation_fleet" {
-  name                 = "sim-fleet"
+  name                 = join("-", [var.prefix, "simulation-fleet"])
   desired_capacity     = 0
-  launch_configuration = "${aws_launch_configuration.simulation_fleet.id}"
+  launch_configuration = aws_launch_configuration.simulation_fleet.id
   max_size             = 0
   min_size             = 0
-  vpc_zone_identifier  = ["subnet-2cdb9e02"]
+
   tag {
     key                 = "Name"
-    value               = "sim-fleet"
+    value               = join("-", [var.prefix, "simulation-fleet"])
     propagate_at_launch = true
   }
 }
 
-# Amazon Linux 2 Latest AMI
+# amazon linux 2 latest ami
 data "aws_ami" "amazon_linux_2" {
   most_recent = true
   owners      = ["amazon"]
@@ -42,39 +42,38 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
-# Launch Configuration
+# launch configuration for simulation fleet
 resource "aws_launch_configuration" "simulation_fleet" {
-  name_prefix          = "sim-fleet"
+  name_prefix          = join("-", [var.prefix, "simulation-fleet"])
   image_id             = data.aws_ami.amazon_linux_2.id
   instance_type        = var.instance_type
   key_name             = "a-sim"
   iam_instance_profile = "${aws_iam_instance_profile.simulation_fleet.id}"
-  associate_public_ip_address = true
-  user_data                   = "${data.template_file.simulation_fleet.rendered}"
-  ebs_optimized               = true
+  user_data            = "${data.template_file.simulation_fleet.rendered}"
   security_groups = [
-    "${aws_security_group.simulation_fleet.id}"
+    aws_security_group.simulation_fleet.id
   ]
 
   root_block_device {
     volume_type           = "gp2"
-    volume_size           = 50
+    volume_size           = 25
     delete_on_termination = true
   }
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
-### User Data DotNet Linux
+# userdata
 data "template_file" "simulation_fleet" {
   template = "${file("./scripts/sim-userdata.sh")}"
 }
 
-### IAM Resources for Dotnet-Linux Nodes
 
+# iam
 resource "aws_iam_role" "simulation_fleet" {
-  name               = "sim-fleet"
+  name               = join("-", [var.prefix, "simulation-fleet"])
   path               = "/"
   assume_role_policy = <<EOF
 {
@@ -94,26 +93,31 @@ EOF
 }
 
 resource "aws_iam_instance_profile" "simulation_fleet" {
-  name = "sim-fleet"
-  role = "${aws_iam_role.simulation_fleet.name}"
+  name = join("-", [var.prefix, "simulation-fleet"])
+  role = aws_iam_role.simulation_fleet.name
 }
 
 resource "aws_iam_role_policy_attachment" "simulation_fleet" {
-  role       = "${aws_iam_role.simulation_fleet.name}"
+  role       = aws_iam_role.simulation_fleet.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_security_group" "simulation_fleet" {
-  name        = "simulation_fleet_ssh"
-  description = "Allow ssh to sim_fleet"
-  vpc_id      = "${var.vpc_id}"
+  name        = join("-", [var.prefix, "simulation-fleet"])
+  description = "Allow access to simulation fleet"
+  vpc_id      = var.vpc_id
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["172.249.71.28/32", "38.32.68.178/32", "73.221.37.117/32"]
+  dynamic "ingress" {
+    for_each = var.whitelist_ips
+
+    content {
+      from_port   = ingress.value["port"]
+      to_port     = ingress.value["port"]
+      protocol    = "tcp"
+      cidr_blocks = [ingress.value["cidr"]]
+    }
   }
+
   egress {
     from_port   = 0
     to_port     = 0
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,3 +10,8 @@ variable "profile" {
 variable "instance_type" {
   default = "m5.large"
 }
+variable "whitelist_ips" {
+  type        = list(map(string))
+  default     = []
+  description = "List of cidr blocks and ports to create ingress rules for."
+}
