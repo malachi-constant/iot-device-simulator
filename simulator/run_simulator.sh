97d6fa572b25670979985e75f37978038f57f550
Lucas Hanson <murdoc@rosewater.local>
2020-05-12 21:44:44 -0700

    cleanup

--- simulator/run_simulator.sh
+++ simulator/run_simulator.sh
@@ -21,7 +21,7 @@ do
     echo '[*] Running next ' $BATCH_NUMBER ' simulations in five seconds...'
     sleep 5
   fi
-  OUTPUT=$(python3 mojio-sim.py --region us-east-1 --iot-endpoint a28t31er3mx77a-ats.iot.us-east-1.amazonaws.com --simulation-table simulation-table --iot-topic connectedcar/trip/test > /tmp/$i.log ) & done
+  OUTPUT=$(python3 clevertime-sim.py --region us-east-1 --iot-endpoint a28t31er3mx77a-ats.iot.us-east-1.amazonaws.com --simulation-table simulation-table --iot-topic connectedcar/trip/test > /tmp/$i.log ) & done
 
 
 echo '[*] Started all simulations successfully'
