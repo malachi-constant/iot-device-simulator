6604a757e1a7a8c5cb65e1d8cc9b4833421e5ca1
Lucas Hanson <murdoc@rosewater.local>
2020-05-13 13:17:18 -0700

    separating out get data, from validate

--- simulator/__main__.py
+++ simulator/__main__.py
@@ -1,7 +1,7 @@
 import boto3
 import json
 import random
-from datetime import datetime, timedelta, tzinfo
+import data_generator
 import time
 import argparse
 import sys
@@ -10,11 +10,12 @@ import os
 
 # get program arguments
 parser = argparse.ArgumentParser(description='IoT Device Simulator Built for clevertime Sample Data')
-parser.add_argument('--region', dest='region', required=False, default='us-west-2',help='Specify the AWS Region')
-parser.add_argument('--iot-endpoint', dest='iot_endpoint', required=False, default='a28t31er3mx77a-ats.iot.us-west-2.amazonaws.com',help='Specify the AWS IoT Core Endpoint to publish to.')
-parser.add_argument('--simulation-table', dest='simulation_table', required=False, default='simulation-table',help='Specify a DynamoDB Table for storing simulation state.')
-parser.add_argument('--iot-topic', dest='iot_topic', required=False, default='clevertime/simulator_rule',help='Specify a IoT Topic to Publish to.')
+parser.add_argument('--region','-r', dest='region', required=False, default='us-west-2',help='Specify the AWS Region')
+parser.add_argument('--iot-endpoint','-e', dest='iot_endpoint', required=False, default='a28t31er3mx77a-ats.iot.us-west-2.amazonaws.com',help='Specify the AWS IoT Core Endpoint to publish to.')
+parser.add_argument('--simulation-table','-T', dest='simulation_table', required=False, default='simulation-table',help='Specify a DynamoDB Table for storing simulation state.')
+parser.add_argument('--iot-topic','-t', dest='iot_topic', required=False, default='test',help='Specify a IoT Topic to Publish to.')
 parser.add_argument('--data','-d' ,dest='data', required=False, default='sample',help='Data schema file to use.')
+parser.add_argument('--interval','-i' ,dest='message_interval', required=False, default=1,help='Message Interval in seconds.')
 parser.add_argument('--profile', dest='profile', required=False,help='AWS Profile to use.')
 
 args = parser.parse_args()
@@ -26,131 +27,12 @@ iot_topic         = args.iot_topic
 region            = args.region
 profile           = args.profile
 simulation_table  = args.simulation_table
+message_interval  = args.message_interval
 data_location     = "./data/" + args.data + ".json"
 dynamodb          = boto3.resource('dynamodb', region_name=region)
 valid_types       = ["float", "int", "bool", "string"]
 valid_field_attributes = {"float":{"type":"string","from":"float","to":"float","average":"float","mode":"string"},"int":{"type":"string","from":"float","to":"float","average":"float","mode":"string"}, "bool":{"type":"string","weight":"float"}, "string":{"type":"string","possibilities":"string"}}
 
-def exit_handler():
-    print ('My application is ending!')
-
-
-class FixedOffset(tzinfo):
-    """offset_str: Fixed offset in str: e.g. '-0400'"""
-    def __init__(self, offset_str):
-        sign, hours, minutes = offset_str[0], offset_str[1:3], offset_str[4:]
-        offset = (int(hours) * 60 + int(minutes)) * (-1 if sign == "-" else 1)
-        self.__offset = timedelta(minutes=offset)
-        # NOTE: the last part is to remind about deprecated POSIX GMT+h timezones
-        # that have the opposite sign in the name;
-        # the corresponding numeric value is not used e.g., no minutes
-        '<%+03d%02d>%+d' % (int(hours), int(minutes), int(hours)*-1)
-    def utcoffset(self, dt=None):
-        return self.__offset
-    def tzname(self, dt=None):
-        return self.__name
-    def dst(self, dt=None):
-        return timedelta(0)
-    def __repr__(self):
-        return 'FixedOffset(%d)' % (self.utcoffset().total_seconds() / 60)
-
-def convertTimestamp(timestamp):
-    date_with_tz = timestamp
-    date_str, tz = date_with_tz[:-6], date_with_tz[-6:]
-    dt_utc = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
-    dt = dt_utc.replace(tzinfo=FixedOffset(tz))
-    return dt
-
-
-def findMSDifference (start_time, end_time):
-    start_timestamp = convertTimestamp(start_time)
-    end_timestamp = convertTimestamp(end_time)
-    duration_time = end_timestamp - start_timestamp;
-    duration_ms = duration_time.seconds * 1000
-
-    return duration_ms;
-
-### Parse Trip File and create JSON
-def parseData(trip_data, trip_selection) :
-    data_JSON = []
-    timingArray = []
-    device_id = str(random.randint(10000,99999)) ## Generate device id for simulation
-    trip_id   = uuid.uuid4().hex
-    lines = trip_data.split('\n')
-    line_number = len(lines);
-
-
-    for i in range(line_number-1) :
-        columns = lines[i].strip().split(',');
-        if (i > 1):
-            previous_columns = lines[i-1].strip().split(',')
-            timing_array.append(findMSDifference(previous_columns[1], columns[1]))
-        dev = [];
-        message_id = uuid.uuid4().hex
-        dev.append({
-          "message_id": message_id,
-          "trip_id": trip_id,
-          "device_id": device_id,
-          "devicetime": columns[1],
-          "eventId": columns[2],
-          ## dev_acceleration: columns[3],
-          ## dev_accl_magnitude: columns[4],
-          ## dev_accl_x_force : columns[5],
-          ## dev_accl_y_force: columns[6],
-          ## dev_accl_z_force: columns[7],
-          "dev_ext_bat_voltage": columns[8],
-          "dev_int_bat_voltage": columns[9],
-          #"dev_nw_carrier": columns[10],
-          "dev_nw_cell_sig_str": columns[11],
-          "dev_nw_conn_roaming": columns[12],
-          "dev_nw_conn_type": columns[13],
-          "veh_coolant_temp": columns[14],
-          "veh_dtc_count": columns[15],
-          "veh_engine_fuel_rate": columns[16],
-          "veh_engine_load": columns[17],
-          "veh_engine_oil_temp": columns[18],
-          "veh_fuel_level": columns[19],
-          "veh_fuel_pressure": columns[20],
-          "veh_ima_pressure": columns[21],
-          "veh_intake_air_temp": columns[22],
-          "veh_maf_air_flow": columns[23],
-          "veh_mil_status": columns[24],
-          "veh_real_odo": columns[25],
-          "veh_rpm": columns[26],
-          "veh_speed": columns[27],
-          "veh_throttle_pos": columns[28],
-          "veh_tire_lf_pressure": columns[29],
-          "veh_tire_lr_pressure": columns[30],
-          "veh_tire_rf_pressure": columns[31],
-          "veh_tire_rr_pressure": columns[32],
-          "veh_virt_odo": columns[33],
-          "gps_alt": columns[34],
-          "gps_fix_status": columns[35],
-          "gps_fix_time": columns[36],
-          "gps_hacc": columns[37],
-          "gps_hdop": columns[38],
-          "gps_heading": columns[39],
-          "gps_lat": columns[40],
-          "gps_lon": columns[41],
-          "gps_lost_lock_duration": columns[42],
-          "gps_lost_lock_time": columns[43],
-          "gps_num_sat": columns[44],
-          "gps_pdop": columns[45],
-          "gps_speed": columns[46],
-          "gps_vacc": columns[47],
-          "gps_vdop": columns[48]})
-
-        data_JSON.append(dev);
-
-
-        if (columns[2] == "VEH_TRIP_MOVING_STOPPED") :
-            ### Get timestamp and difference in milliseconds between first timestamp and last return that as an interval
-            first_timestamp = lines[1].strip().split(',')[1]
-            simulationDuration = findMSDifference(first_timestamp,columns[1]);
-
-    print('[*] Simulation Duration: ' + str(simulationDuration/1000) + " seconds")
-
-    return data_JSON
 
 def run_simulator():
     # set aws profile
@@ -226,24 +108,7 @@ def run_simulator():
 
 
 # validate json schema
-def validate_data(data_location):
-    print("[*] data file: " + data_location)
-
-    # open file
-    try:
-        file  = open(data_location,"r")
-    except:
-        print("[!] schema file at " + data_location + " does not exist")
-        exit()
-    file_data = file.read()
-
-    # json lint
-    try:
-        json_data = json.loads(file_data)
-    except:
-        print("[!] schema file at " + data_location + " is not valid.json.")
-        exit()
-    print("[*] json is valid")
+def validate_data(json_data):
 
     # check schema
     print("[*] validating fields...")
@@ -282,6 +147,26 @@ def validate_data(data_location):
 
     return True
 
+def open_data(data_location):
+    print("[*] data file: " + data_location)
+
+    # open file
+    try:
+        file  = open(data_location,"r")
+    except:
+        print("[!] schema file at " + data_location + " does not exist")
+        exit()
+    file_data = file.read()
+
+    # json lint
+    try:
+        json_data = json.loads(file_data)
+    except:
+        print("[!] schema file at " + data_location + " is not valid.json.")
+        exit()
+    print("[*] json is valid")
+    return json_data
+
 # welcome banner
 def welcome():
     print("[*] welcome to the iot device simulator !\n")
@@ -292,11 +177,15 @@ def main():
 
     welcome()
 
-    if validate_data(data_location):
+    schema = open_data(data_location)
+
+    if validate_data(schema):
         print("[*] schema is valid")
     else:
         print("[!] exiting...")
         exit()
+
+    data_generator.generate(schema)
     # try:
     #     run_simulator()
     # except KeyboardInterrupt:
---
+++ simulator/data_generator.py
@@ -0,0 +1,4 @@
+import json
+
+def generate(schema):
+    print(schema)
